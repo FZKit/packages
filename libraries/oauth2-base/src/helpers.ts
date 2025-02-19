0b939f37a1eecf1db2e1e9ce624e7a899ee883e7
Maurício Sousa <morricedev@gmail.com>
2025-02-19 17:06:19 -0300

    feat(oauth2-base): add helpers to handle auth start redirect and auth callback

--- libraries/oauth2-base/src/helpers.ts
+++ libraries/oauth2-base/src/helpers.ts
@@ -1,4 +1,8 @@
-import { sessionClients } from './globals';
+import type { OAuth2Namespace } from "@fastify/oauth2";
+import type { FastifyInstance, FastifyRequest } from "fastify";
+import type { OAuth2BaseConfigInstance } from "./base-config";
+import { sessionClients } from "./globals";
+import type { UserData } from "./user-data";
 
 export function sseSendJsonData({
   sessionId,
@@ -17,4 +21,98 @@ export function sseSendJsonData({
       channel.end();
     }
   }
+}
+
+export function setupStartRedirect<T extends FastifyInstance>(
+  scope: T,
+  options: {
+    startRedirectPath: string;
+    cookiePath: string;
+    namespace: keyof T;
+  }
+) {
+  scope.get<{ Params: { sessionId: string } }>(
+    `${options.startRedirectPath}/:sessionId`,
+    async (request, reply) => {
+      const namespace = scope[options.namespace] as unknown as OAuth2Namespace;
+      if (!namespace) {
+        throw new Error(`Namespace ${namespace} not found`);
+      }
+      const uri = await namespace.generateAuthorizationUri(request, reply);
+      reply
+        .setCookie("session_id", request.params.sessionId, {
+          httpOnly: true,
+          sameSite: "lax",
+          path: options.cookiePath,
+        })
+        .redirect(`${uri}&session_id=${request.params.sessionId}`);
+    }
+  );
+}
+
+export function callbackExecutor<T extends OAuth2BaseConfigInstance>(
+  scope: T,
+  callback: (
+    request: FastifyRequest
+  ) => Promise<{ parsed: UserData; raw: unknown }>,
+  options: { callbackPath: string; cookiePath: string }
+) {
+  scope.get(options.callbackPath, async (request, reply) => {
+    const sessionId = request.cookies.session_id;
+    try {
+      const { parsed, raw } = await callback(request);
+      if (scope.dataProcessor) {
+        await scope.dataProcessor({
+          data: parsed,
+          request,
+          reply,
+          sseDispatcher: (callbackData) =>
+            sseSendJsonData({
+              sessionId,
+              data: callbackData,
+            }),
+        });
+        return;
+      }
+      sseSendJsonData({
+        sessionId,
+        data: parsed,
+      });
+      if (scope.successRedirectPath) {
+        return reply.redirect(scope.successRedirectPath);
+      }
+      return reply.send(raw);
+    } catch (e) {
+      const defaultErrorMessage = "Failed to get user data";
+      const isInstanceOfError = e instanceof Error;
+      const errorObject = {
+        error: isInstanceOfError ? e.message : defaultErrorMessage,
+      };
+      const errorInstance = isInstanceOfError
+        ? e
+        : new Error(defaultErrorMessage);
+      if (scope.errorProcessor) {
+        await scope.errorProcessor({
+          error: errorInstance,
+          request,
+          reply,
+          sseDispatcher: (callbackData) =>
+            sseSendJsonData({
+              sessionId,
+              data: callbackData,
+            }),
+        });
+        return;
+      }
+      sseSendJsonData({
+        sessionId,
+        data: errorObject,
+      });
+      if (scope.failureRedirectPath) {
+        scope.setFailureException(errorInstance);
+        return reply.redirect(scope.failureRedirectPath);
+      }
+      return reply.code(400).send(errorObject);
+    }
+  });
 }
--- libraries/oauth2-google/src/index.ts
+++ libraries/oauth2-google/src/index.ts
@@ -2,12 +2,14 @@ import oauthPlugin, { type OAuth2Namespace } from "@fastify/oauth2";
 import { httpClient } from "@fzkit/base/http-client";
 import { FZKitPlugin, createFastifyPlugin } from "@fzkit/base/plugin";
 import {
+  type GoogleUserData,
   OAuth2BaseConfigFZKitPlugin,
   type OAuth2BaseConfigInstance,
   type UserData,
-  sseSendJsonData,
+  callbackExecutor,
+  setupStartRedirect,
 } from "@fzkit/oauth2-base";
-import type { FastifyInstance } from "fastify";
+import type { FastifyInstance, FastifyRequest } from "fastify";
 
 export interface GoogleOAuth2PluginInstance
   extends FastifyInstance,
@@ -45,8 +47,15 @@ class GoogleOAuth2FZKitPlugin extends FZKitPlugin<
     options.startRedirectPath = startRedirectPath;
     options.cookiePath = cookiePath;
     this.registerClient(scope, { ...options, callbackUri });
-    this.setupStartRedirect(scope, options);
-    this.setupCallback(scope, { ...options, callbackUri });
+    setupStartRedirect(scope, {
+      cookiePath: options.cookiePath,
+      namespace: "googleOAuth2",
+      startRedirectPath: options.startRedirectPath,
+    });
+    callbackExecutor(scope, (request) => this.setupCallback(scope, request), {
+      callbackPath: new URL(callbackUri).pathname,
+      cookiePath: options.cookiePath,
+    });
     return Promise.resolve();
   }
 
@@ -72,104 +81,25 @@ class GoogleOAuth2FZKitPlugin extends FZKitPlugin<
     });
   }
 
-  private setupStartRedirect(
+  private async setupCallback(
     scope: GoogleOAuth2PluginInstance,
-    options: GoogleOAuth2PluginOptions
-  ) {
-    scope.get<{ Params: { sessionId: string } }>(
-      `${options.startRedirectPath}/:sessionId`,
-      async (request, reply) => {
-        const uri = await scope.googleOAuth2.generateAuthorizationUri(
-          request,
-          reply
-        );
-        reply
-          .setCookie("sessionId", request.params.sessionId, {
-            httpOnly: true,
-            sameSite: "lax",
-            path: options.cookiePath,
-          })
-          .redirect(`${uri}&sessionId=${request.params.sessionId}`);
+    request: FastifyRequest
+  ): Promise<{ parsed: GoogleUserData; raw: unknown }> {
+    const { token } =
+      await scope.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
+    const response = await httpClient.get(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${token.access_token}`,
+        },
       }
     );
-  }
-
-  private setupCallback(
-    scope: GoogleOAuth2PluginInstance,
-    options: GoogleOAuth2PluginOptions & { callbackUri: string }
-  ) {
-    const callBackPath = new URL(options.callbackUri).pathname;
-    scope.get(callBackPath, async (request, reply) => {
-      const sessionId = request.cookies.sessionId;
-      try {
-        const { token } =
-          await scope.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(
-            request
-          );
-        const response = await httpClient.get(
-          "https://www.googleapis.com/oauth2/v2/userinfo",
-          {
-            headers: {
-              Authorization: `Bearer ${token.access_token}`,
-            },
-          }
-        );
-        const rawData = await response.json();
-        const data = { basicInfo: rawData, provider: "google" } as UserData;
-        if (scope.dataProcessor) {
-          await scope.dataProcessor({
-            data,
-            request,
-            reply,
-            sseDispatcher: (callbackData) =>
-              sseSendJsonData({
-                sessionId,
-                data: callbackData,
-              }),
-          });
-          return;
-        }
-        sseSendJsonData({
-          sessionId,
-          data,
-        });
-        if (scope.successRedirectPath) {
-          return reply.redirect(scope.successRedirectPath);
-        }
-        return reply.send(rawData);
-      } catch (e) {
-        const defaultErrorMessage = "Failed to get user data";
-        const isInstanceOfError = e instanceof Error;
-        const errorObject = {
-          error: isInstanceOfError ? e.message : defaultErrorMessage,
-        };
-        const errorInstance = isInstanceOfError
-          ? e
-          : new Error(defaultErrorMessage);
-        if (scope.errorProcessor) {
-          await scope.errorProcessor({
-            error: errorInstance,
-            request,
-            reply,
-            sseDispatcher: (callbackData) =>
-              sseSendJsonData({
-                sessionId,
-                data: callbackData,
-              }),
-          });
-          return;
-        }
-        sseSendJsonData({
-          sessionId,
-          data: errorObject,
-        });
-        if (scope.failureRedirectPath) {
-          scope.setFailureException(errorInstance);
-          return reply.redirect(scope.failureRedirectPath);
-        }
-        return reply.code(400).send(errorObject);
-      }
-    });
+    const rawData = await response.json();
+    return {
+      parsed: { basicInfo: rawData, provider: "google" } as UserData,
+      raw: rawData,
+    };
   }
 }
 
