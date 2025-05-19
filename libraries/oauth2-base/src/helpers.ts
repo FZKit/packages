a4f0136f53676a257acc7952dd04fa36286e3e4c
Maurício Sousa <morricedev@gmail.com>
2025-05-19 14:59:05 -0300

    feat(setupStartRedirect): set state cookie with correct cookie settings

--- libraries/oauth2-base/src/helpers.ts
+++ libraries/oauth2-base/src/helpers.ts
@@ -42,14 +42,22 @@ export function setupStartRedirect<T extends FastifyInstance>(
         throw new Error(`Namespace ${namespace} not found`);
       }
       const uri = await namespace.generateAuthorizationUri(request, reply);
-      reply
-        .setCookie('session_id', request.params.sessionId, {
+      reply.setCookie('session_id', request.params.sessionId, {
+        httpOnly: true,
+        sameSite: options.cookieSameSiteNone ? 'none' : 'lax',
+        secure: options.cookieSameSiteNone,
+        path: options.cookiePath,
+      });
+      const searchParams = new URLSearchParams(uri.split('?')[1]);
+      if (options.cookieSameSiteNone) {
+        reply.setCookie('oauth2-redirect-state', searchParams.get('state') ?? '', {
           httpOnly: true,
           sameSite: options.cookieSameSiteNone ? 'none' : 'lax',
           secure: options.cookieSameSiteNone,
           path: options.cookiePath,
-        })
-        .redirect(`${uri}&session_id=${request.params.sessionId}`);
+        });
+      }
+      reply.redirect(`${uri}&session_id=${request.params.sessionId}`);
     },
   );
 }
